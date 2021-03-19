3763cfd2918d4e0095ba747b3477f2351f99caa6
Alexey <alexeyp0708@gmail.com>
2021-03-19 15:13:02 +0500

    change HandlersClass

--- src/HandlersClass.php
+++ src/HandlersClass.php
@@ -7,4 +7,9 @@ namespace Alpa\ProxyObject;
 abstract class HandlersClass implements HandlersContract
 {
     use HandlersStaticMethods;
+
+    public function run(string $action,object $target,?string $prop=null,$value_or_arguments=null,Proxy $proxy)
+    {
+        
+    }
 }
